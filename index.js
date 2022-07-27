ab9872a1e994b2456d529dfc70d5073ff2992c6c
Mr-Abbosbek <abbosbekmahmudjonov2001@gmail.com>
2022-07-27 23:34:49 +0500

    make it better

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8888
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+})
+
+app.listen(port, ()=>{
+  console.log(`App listen on port http://localhost:${port}`);
+})
