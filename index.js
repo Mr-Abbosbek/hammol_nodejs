ac8ee93b16259bca1a9372834555574a00a0b5b4
Mr-Abbosbek <abbosbekmahmudjonov2001@gmail.com>
2022-07-28 10:26:27 +0500

    make it better

--- index.js
+++ index.js
@@ -570,6 +570,56 @@ app.get("/api", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 })
 
+app.get('/api/product', (req, res)=>{
+  let {name = '', category = '', offset = 0, limit = 10} = req.query
+  console.log("BEFORE TRIM", name, category);
+  name = name.toLowerCase()
+  category = category.toLowerCase()
+  console.log("AFTER TRIM", name, category);
+  if(!limit) {
+      limit = 10
+  }
+
+  if(!name && !category) {
+      // res.json(products.products)
+      console.log("not given");
+      let found = products.products
+      let count = found.slice(offset, limit)
+      res.json({
+          products: count,
+          count: found.length
+
+      })
+
+  } else if( !name && category ) {
+      let found = products.products.filter(el=> el.category == category)
+      console.log('category given');
+      let count = found.slice(offset, limit)
+      res.json({
+          products: count,
+          count: found.length
+      })
+      return
+  } else if (name && !category) {
+      let found = products.products.filter(el=> el.title.toLowerCase().includes(name))
+      console.log('name given');
+      let count = found.slice(offset, limit)
+      res.json({
+          products: count,
+          count: found.length
+      })
+      return
+  } else {
+      let found = products.products.filter(el=> el.title.toLowerCase().includes(name) && el.category == category)
+      console.log('all given');
+      let count = found.slice(offset, limit)
+      res.json({
+          products: count,
+          count: found.length
+      })
+  }
+})
+
 app.get('/api/product/:id', (req, res)=>{
   let product = products.products.find(el=> el.id == req.params.id)
   if (!product) {
